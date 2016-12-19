8e7e72cf74917ebcfd050f524bf4228aa524ccd4
Alan Martins <alan.m21@hotmail.com>
2016-12-18 22:39:03 -0200

    Obtendo token e criando notificação

--- app/src/main/java/com/firebasenotificationmobile/FirebaseMessagingService.java
+++ app/src/main/java/com/firebasenotificationmobile/FirebaseMessagingService.java
@@ -1,5 +1,11 @@
 package com.firebasenotificationmobile;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.firebase.messaging.RemoteMessage;
 
 /**
@@ -12,5 +18,22 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
         String message = remoteMessage.getData().get("message");
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setContentTitle("Teste")
+                .setContentText( message )
+                .setContentIntent( pi )
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+
     }
 }
